e8778ca60d96d4b352391f71c6ed852dfb83e068
Алексей Панцерновский <pan_alexey@mail.ru>
2024-09-22 11:13:46 +0600

    Добавлен запрос для получения моковых данных с JSON-сервера.

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,19 +1,39 @@
+import got from 'got';
+import { TMockServerData } from '../../shared/types/index.js';
 import { ICommand } from './command.interface.js';
 
 export class GenerateCommand implements ICommand {
+  private initialData: TMockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, usersFileName, offersFileName, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
-    // Код для получения данных с сервера.
-    // Формирование файлов с пользователями и предложениями по аренде.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+
     console.log(usersFileName);
     console.log(offersFileName);
     console.log(offerCount);
-    console.log(url);
+    console.log(this.initialData);
   }
 }
