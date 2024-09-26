76006de3cc92a5fc383e217195d2ef00f4f61e8f
Алексей Панцерновский <pan_alexey@mail.ru>
2024-09-27 01:37:47 +0600

    Добавлен Config в RestApplication

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,11 +1,14 @@
 import { ILogger } from '../shared/libs/logger/index.js';
+import { IConfig } from '../shared/libs/config/index.js';
 
 export class RestApplication {
   constructor(
-    private readonly logger: ILogger
+    private readonly logger: ILogger,
+    private readonly config: IConfig
   ) {}
 
   public async init() {
     this.logger.info('Приложение инициализировано');
+    this.logger.info(`Значение из env $PORT: ${this.config.get('PORT')}`);
   }
 }
