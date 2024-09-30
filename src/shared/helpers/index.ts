d61ffe1e7db6782f40760b92a95cd23a252da8f9
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-01 00:24:00 +0600

    4.6. Добавит функцию-хелпер getMongoURI

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,3 +1,4 @@
 
-export {generateRandomValue, getRandomItems, getRandomItem, getErrorMessage} from './common.js';
+export { generateRandomValue, getRandomItems, getRandomItem, getErrorMessage } from './common.js';
 export { getCurrentModuleDirectoryPath } from './file-system.js';
+export { getMongoURI } from './database.js';
