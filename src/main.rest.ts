d57d16f3cc6109c64ea4f9ab46dda6e0c8a4ed8a
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-01 00:30:06 +0600

    4.8. Добавит привязку для DatabaseClient

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,19 +1,20 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-
 import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
 import { IConfig, RestConfig, TRestSchema } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
+import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+
 
 async function bootstrap() {
   const container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
   container.bind<IConfig<TRestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+  container.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
-
 bootstrap();
