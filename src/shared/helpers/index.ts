f0991c4df8359666fe29946428b7a42895ff5da4
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-01 01:32:38 +0600

    4.20. Добавит методы в UserEntity. WIP

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -2,3 +2,4 @@
 export { generateRandomValue, getRandomItems, getRandomItem, getErrorMessage } from './common.js';
 export { getCurrentModuleDirectoryPath } from './file-system.js';
 export { getMongoURI } from './database.js';
+export { createSHA256 } from './hash.js';
--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,10 +1,17 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
-
-import { EMPTY_AVATAR } from '../../../const/data.js';
+import { defaultClasses, getModelForClass, prop, modelOptions } from '@typegoose/typegoose';
 import { TUser } from '../../types/index.js';
+import { EMPTY_AVATAR } from '../../../const/data.js';
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  }
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements TUser {
@@ -22,6 +29,23 @@ export class UserEntity extends defaultClasses.TimeStamps implements TUser {
 
   @prop({ required: true })
   public isPro: boolean;
+
+  constructor(userData: TUser) {
+    super();
+
+    this.name = userData.name;
+    this.email = userData.email;
+    this.avatarUrl = userData.avatarUrl;
+    this.isPro = userData.isPro;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
