bef13c17355187b5e6ff3e752611f9d21491ae0a
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-01 01:21:18 +0600

    4.19. Добавит поля _id и id. WIP

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -3,6 +3,10 @@ import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
 import { EMPTY_AVATAR } from '../../../const/data.js';
 import { TUser } from '../../types/index.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface UserEntity extends defaultClasses.Base {}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements TUser {
   @prop({ trim: true, required: true, default: '' })
   public name: string;
