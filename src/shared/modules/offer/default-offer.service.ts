7805ab362c05b315ec2d39c8a7da33f969bca749
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-08 23:10:10 +0600

    Добавлен объект c автором предложения из связанной коллекции пользователей

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -11,7 +11,8 @@ import { CreateOfferDto } from './dto/create-offer.dto.js';
 export class DefaultOfferService implements IOfferService {
   constructor(
     @inject(Component.Logger) private readonly logger: ILogger,
-    @inject(Component.OfferModel) private readonly offerModel: types.ModelType<OfferEntity>
+    @inject(Component.OfferModel)
+    private readonly offerModel: types.ModelType<OfferEntity>
   ) {}
 
   public async create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>> {
@@ -21,7 +22,9 @@ export class DefaultOfferService implements IOfferService {
     return result;
   }
 
-  public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+  public async findById(
+    offerId: string
+  ): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel.findById(offerId).populate(['hostId']).exec();
   }
 }
