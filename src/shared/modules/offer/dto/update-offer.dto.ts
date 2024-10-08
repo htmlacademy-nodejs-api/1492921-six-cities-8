163c64cfa7563317d742e44271cba597ce4a192f
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-08 23:37:26 +0600

    Расширен интерфейс OfferService

---
+++ src/shared/modules/offer/dto/update-offer.dto.ts
@@ -0,0 +1,25 @@
+import {
+  TCity,
+  TOfferGoods,
+  TOfferType,
+  TPoint,
+} from '../../../types/index.js';
+
+export class UpdateOfferDto {
+  public title?: string;
+  public description?: string;
+  public date?: string;
+  public city?: TCity;
+  public previewImage?: string;
+  public images?: string[];
+  public isPremium?: boolean;
+  public isFavorite?: boolean;
+  public rating?: number;
+  public type?: TOfferType;
+  public bedrooms?: number;
+  public maxAdults?: number;
+  public price?: number;
+  public goods?: TOfferGoods[];
+  public hostId?: string;
+  public location?: TPoint;
+}
--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -2,8 +2,20 @@ import { DocumentType } from '@typegoose/typegoose';
 
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { OfferEntity } from './offer.entity.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { TCityName } from '../../types/city.type.js';
 
 export interface IOfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(
+    offerId: string,
+    dto: UpdateOfferDto
+  ): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findFavorites(): Promise<DocumentType<OfferEntity>[]>;
+  findPremium(cityName: TCityName): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
 }
