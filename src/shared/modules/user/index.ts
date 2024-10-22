bb3f986354afd010eb4fc7b29f3df07f20f70d30
Алексей Панцерновский <pan_alexey@mail.ru>
2024-10-22 12:26:52 +0600

    Добавлен метод uploadAvatar в UserController

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -10,6 +10,7 @@ export { CreateUserDto } from './dto/create-user.dto.js';
 export { UpdateUserDto } from './dto/update-user.dto.js';
 export { LoginUserDto } from './dto/login-user.dto.js';
 export { UserRdo } from './rdo/user.rdo.js';
+export { UploadAvatarUserRdo } from './rdo/upload-avatar-user.rdo.js';
 export { DefaultUserService } from './default-user.service.js';
 export { createUserContainer } from './user.container.js';
 export { IUserService } from './user-service.interface.js';
